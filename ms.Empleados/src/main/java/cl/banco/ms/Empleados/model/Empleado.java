878a71ef16e1395f139959280a09220a712be0ef
Gabriel Garrido <ga.garridog@duocuc.cl>
2026-05-15 19:07:52 -0400

    Se corrige modelo Empleado usando Lombok

--- ms.Empleados/src/main/java/cl/banco/ms/Empleados/model/Empleado.java
+++ ms.Empleados/src/main/java/cl/banco/ms/Empleados/model/Empleado.java
@@ -2,8 +2,17 @@ package cl.banco.ms.Empleados.model;
 
 import jakarta.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "empleados")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Empleado {
 
     @Id
@@ -33,103 +42,5 @@ public class Empleado {
     @Column(nullable = false)
     private String area;
 
-    // DISPONIBILIDAD
     private Boolean disponible;
-
-    public Empleado() {
-    }
-
-    public Empleado(Integer id,
-                     String rut,
-                     String nombre,
-                     String apellido,
-                     String correo,
-                     String cargo,
-                     String sucursal,
-                     String area,
-                     Boolean disponible) {
-
-        this.id = id;
-        this.rut = rut;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.correo = correo;
-        this.cargo = cargo;
-        this.sucursal = sucursal;
-        this.area = area;
-        this.disponible = disponible;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRut() {
-        return rut;
-    }
-
-    public void setRut(String rut) {
-        this.rut = rut;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public String getSucursal() {
-        return sucursal;
-    }
-
-    public void setSucursal(String sucursal) {
-        this.sucursal = sucursal;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public Boolean getDisponible() {
-        return disponible;
-    }
-
-    public void setDisponible(Boolean disponible) {
-        this.disponible = disponible;
-    }
 }
-
