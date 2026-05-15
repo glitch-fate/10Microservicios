7e8cca2808cd4bee77ac69afac47255f15d84fdc
Raul Jara <ra.jarav@duocuc.cl>
2026-05-15 14:03:08 -0400

    se agrego el @service

--- msClientes/msClientes/msClientes/src/main/java/cl/banco/msClientes/repository/DireccionRepository.java
+++ msClientes/msClientes/msClientes/src/main/java/cl/banco/msClientes/repository/DireccionRepository.java
@@ -1,8 +1,10 @@
 package cl.banco.msClientes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cl.banco.msClientes.model.Direccion;
 
+@Repository
 public interface DireccionRepository extends JpaRepository<Direccion, Integer> {
 }
